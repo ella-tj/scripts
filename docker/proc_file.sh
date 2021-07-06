22acd71b65e73b51d850f53942385862b1d239c6
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-07-06 19:28:32 +0800

    默认不运行前端扫码

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -25,3 +25,20 @@ else
       echo "已配置不启用jd_crazy_joy_coin任务，仅杀掉"
    fi
 fi
+
+echo "处理前端扫码"
+if [ ! $QRcookie_ENABLE ]; then
+      eval $(ps -ef | grep "index" | grep -v "grep" | awk '{print "kill "$1}')
+      echo "默认不启用前端扫码任务，仅杀掉"
+else
+   if [ $QRcookie_ENABLE = "Y" ]; then
+      echo "配置启用前端扫码，杀掉QRcookie任务，并重启"
+      eval $(ps -ef | grep "index" | grep -v "grep" | awk '{print "kill "$1}')
+      echo '' >/scripts/logs/QRcookie.log
+      $CMD /scripts/QRcookie/index.js | ts >>/scripts/logs/QRcookie.log 2>&1 &
+      echo "配置前端扫码重启完成"
+   else
+      eval $(ps -ef | grep "index" | grep -v "grep" | awk '{print "kill "$1}')
+      echo "已配置不启用前端扫码任务，仅杀掉"
+   fi
+fi
