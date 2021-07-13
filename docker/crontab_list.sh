359ed64d0af550832a72009040f9dbe93d7c508d
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-07-13 22:30:55 +0800

    fix

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -152,9 +152,9 @@
 #挖现金
 #0 0 * * * node /scripts/m_wxj.js >> /scripts/logs/m_wxj.log 2>&1
 #领现金助力
-#0 0 * * * python3 /scripts/m_cashHelp.py >> /scripts/logs/m_cashHelp.log 2>&1
+0 0 * * * python3 /scripts/m_cashHelp.py >> /scripts/logs/m_cashHelp.log 2>&1
 #狂欢欧洲杯
-0 10 * * * node /scripts/m_europeancup.js >> /scripts/logs/m_europeancup.log 2>&1
+#0 10 * * * node /scripts/m_europeancup.js >> /scripts/logs/m_europeancup.log 2>&1
 #粉丝互动
 1 8 * * * node /scripts/m_wxFans.js >> /scripts/logs/m_wxFans.log 2>&1
 #锦鲤
