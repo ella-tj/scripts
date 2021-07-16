6ec10f76e10663fb8775454d8a5eb29ed65db20b
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-07-16 10:39:01 +0800

    财富岛热气球挂机任务死循环

--- docker/proc_file.sh
+++ docker/proc_file.sh
@@ -31,3 +31,23 @@ if [ -f "/scripts/logs/acpush.jpg" ];then
 else
       echo "图片文件不存在，默认跑路"
 fi
+
+echo "处理财富岛热气球挂机任务。。。"
+if [ ! $CFD_LOOP_ENABLE ]; then
+   echo "默认启用财富岛热气球挂机任务，杀掉jd_cfd_loop任务，并重启"
+   eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+   echo '' >/scripts/logs/jd_cfd_loop.log
+   ts-node /scripts/jd_cfd_loop.ts | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &
+   echo "默认财富岛热气球挂机任务重启完成"
+else
+   if [ $CFD_LOOP_ENABLE = "Y" ]; then
+      echo "配置启用财富岛热气球挂机任务，杀掉jd_crazy_joy_coin任务，并重启"
+      eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+      echo '' >/scripts/logs/jd_cfd_loop.log
+      ts-node /scripts/jd_cfd_loop.ts | ts >>/scripts/logs/jd_cfd_loop.log 2>&1 &
+      echo "配置财富岛热气球挂机任务重启完成"
+   else
+      eval $(ps -ef | grep "jd_cfd_loop" | grep -v "grep" | awk '{print "kill "$1}')
+      echo "已配置不启用财富岛热气球挂机任务，仅杀掉"
+   fi
+fi
