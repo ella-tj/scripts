28b1487371378f8d58c944cdcd75198fe20f581a
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-07-28 21:44:08 +0800

    扫码修复

--- QRcookie/index.js
+++ QRcookie/index.js
@@ -96,7 +96,7 @@ async function step1() {
         `&returnurl=https://wq.jd.com/passport/LoginRedirect?state=${timeStamp}` +
         '&returnurl=https://home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
       'User-Agent':
-        'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
+        'jdapp;iPhone;10.0.8;13.5;e1b0d1280497ac860077212936146d3164d02cc4;network/4g;ADID/4098B1C6-E0D9-47EE-937F-2030243CC386;model/iPhone11,8;addressid/3583295486;appBuild/167741;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
       Host: 'plogin.m.jd.com',
     },
   });
@@ -139,7 +139,7 @@ async function step2(cookiesObj) {
         `&returnurl=https://wqlogin2.jd.com/passport/LoginRedirect?state=${timeStamp}` +
         '&returnurl=//home.m.jd.com/myJd/newhome.action?sceneval=2&ufc=&/myJd/home.action&source=wq_passport',
       'User-Agent':
-        'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
+        'jdapp;iPhone;10.0.8;13.5;e1b0d1280497ac860077212936146d3164d02cc4;network/4g;ADID/4098B1C6-E0D9-47EE-937F-2030243CC386;model/iPhone11,8;addressid/3583295486;appBuild/167741;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
       Host: 'plogin.m.jd.com',
     },
   });
@@ -180,7 +180,7 @@ async function checkLogin(user) {
       'Content-Type': 'application/x-www-form-urlencoded; Charset=UTF-8',
       Accept: 'application/json, text/plain, */*',
       'User-Agent':
-        'jdapp;android;10.0.5;11;0393465333165363-5333430323261366;network/wifi;model/M2102K1C;osVer/30;appBuild/88681;partner/lc001;eufv/1;jdSupportDarkMode/0;Mozilla/5.0 (Linux; Android 11; M2102K1C Build/RKQ1.201112.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/77.0.3865.120 MQQBrowser/6.2 TBS/045534 Mobile Safari/537.36',
+        'jdapp;iPhone;10.0.8;13.5;e1b0d1280497ac860077212936146d3164d02cc4;network/4g;ADID/4098B1C6-E0D9-47EE-937F-2030243CC386;model/iPhone11,8;addressid/3583295486;appBuild/167741;jdSupportDarkMode/0;Mozilla/5.0 (iPhone; CPU iPhone OS 13_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1',
     },
   });
   return response;
