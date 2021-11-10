67785f552cb428dbe006a53c6f1bb0a1df9e94e0
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-11-10 21:20:50 +0800

    Update crontab_list.sh

--- docker/crontab_list.sh
+++ docker/crontab_list.sh
@@ -225,5 +225,7 @@
 1,15,30 0 * * * python3 /mod_scripts/jd_jxgc_tuan.py >> /scripts/logs/jd_jxgc_tuan.log 2>&1
 # 全民抢京豆
 0 0 * 10,11 * python3 /mod_scripts/jd_qjd.py >> /scripts/logs/jd_qjd.log 2>&1
+# 赚京豆
+1,25,50 0 * * * python3 /mod_scripts/jd_zjd.py >> /scripts/logs/jd_zjd.log 2>&1
 # 京东饭粒
 5 1 * 10,11 * python3 /mod_scripts/jd_fanli.py >> /scripts/logs/jd_fanli.log 2>&1
