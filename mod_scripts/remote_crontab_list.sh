33a613142c6c9ea16bf57266ce2e3b46a9e5ac34
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-11-11 14:02:39 +0800

    Update remote_crontab_list.sh

--- mod_scripts/remote_crontab_list.sh
+++ mod_scripts/remote_crontab_list.sh
@@ -4,3 +4,5 @@
 1,15,30 0 * * * python3 /scripts/jd_jxgc_tuan.py >> /scripts/logs/jd_jxgc_tuan.log 2>&1
 # 全民抢京豆
 0 0 * 10,11 * python3 /scripts/jd_qjd.py >> /scripts/logs/jd_qjd.log 2>&1
+# 京东饭粒
+5 14 * 10,11 * python3 /scripts/mod_scripts/jd_fanli.py >> /scripts/logs/jd_fanli.log 2>&1
