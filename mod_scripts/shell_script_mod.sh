83092ae71126c8517d9ec33c0e3edb57fec178b2
sanbing666 <86876676+sanbing666@users.noreply.github.com>
2021-11-11 14:25:44 +0800

    Update shell_script_mod.sh

--- mod_scripts/shell_script_mod.sh
+++ mod_scripts/shell_script_mod.sh
@@ -2,7 +2,7 @@
 
 
 echo "拷贝脚本到/scripts/目录下"
-cp -f /mod_scripts/*_*.js /scripts
-cp -f /mod_scripts/*_*.py /scripts
+cp -f /scripts/mod_scripts/*_*.js /scripts
+cp -f /scripts/mod_scripts/*_*.py /scripts
 echo "自定义增加crontab任务"
-cat /mod_scripts/remote_crontab_list.sh >> /scripts/docker/merged_list_file.sh
+cat /scripts/mod_scripts/remote_crontab_list.sh >> /scripts/docker/merged_list_file.sh
